ce2193409117e61a479a3e906fc4fc74631e59c9
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-07-03 16:48:27 -0700

    Update 05-DiagonalDifference-main.cpp
    Change the body of the code

--- 05-DiagonalDifference-main.cpp
+++ 05-DiagonalDifference-main.cpp
@@ -1,24 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-// Completed diagonalDifference function below
-int diagonalDifference(vector<vector<int>> arr)
-{
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int diagonalDifference(vector<vector<int>> arr) {
     int rightToLeftDiagonal = 0;
     int leftToRightDiagonal = 0;
+    int arrSize = arr.size();
     
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < arrSize; ++i)
     {
-        for (int j = 0; j < arr.size(); j++)
+        for (int j = 0; j < arrSize; ++j)
         {
             if (i == j)
             {
                 rightToLeftDiagonal += arr[i][j];
             }
             
-            if (i + j == arr.size() - 1)
+            if (i + j == arrSize - 1)
             {
                 leftToRightDiagonal += arr[i][j];
             }
@@ -26,27 +35,78 @@ int diagonalDifference(vector<vector<int>> arr)
     }
     
     return abs(rightToLeftDiagonal - leftToRightDiagonal);
-} // end of diagonalDifference
+}
 
-int main(void)
+int main()
 {
-    int vectorSize;
-    cin >> vectorSize;
-    
-    vector<vector<int>> array(vectorSize, vector<int>(vectorSize));
-    
-    // Fill in array from keyboard
-    for (int i = 0; i < vectorSize; i++)
-    {
-        for (int j = 0; j < vectorSize; j++)
-        {
-            cin >> array[i][j];
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < n; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
         }
     }
-    
-    int result = diagonalDifference(array);
-    
-    cout << result << endl;
-    
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
     return 0;
-} // End of main
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}

1cc2cdaf6ebb7048d396743b66109c38dc065683
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-07-03 16:28:40 -0700

    Update 01-SolveMeFirst-main.cpp
    Changed the body of the code

--- 01-SolveMeFirst-main.cpp
+++ 01-SolveMeFirst-main.cpp
@@ -1,20 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int solveMeFirst(int number1, int number2)
-{
-    return number1 + number2;
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+ return a + b;
+  
 }
 
-int main(void)
-{
-    // Two integer number
-    int x, y;
-    cin >> x >> y;
-    
-    int result = solveMeFirst(x, y);
-    
-    cout << result << endl;
-    
-    return 0;
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
 }
